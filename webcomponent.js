0c9ea2f689d41665ead4804dd63387fc33e95736
roadmaptrackerpoc$roadmaptrackerpoc <david.stocker@sap.com>
2020-02-03 15:15:20 +0100

    Update webcomponent.js

--- webcomponent.js
+++ webcomponent.js
@@ -17,7 +17,7 @@
 
         //Fired when the widget is added to the html DOM of the page
         connectedCallback(){
-            this.redraw();
+            
         }
 
          //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
@@ -32,7 +32,7 @@
 
         //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
 		onCustomWidgetAfterUpdate(oChangedProperties) {
-
+            this.redraw();
         }
         
         //When the custom widget is removed from the canvas or the analytic application is closed
@@ -50,17 +50,15 @@
         */
 
         redraw(){
-            if (this._tagText != null){
-                if (this._tagContainer){
-                    this._tagContainer.parentNode.removeChild(this._tagContainer);
-                }
-
-                var shadow = window.getSelection(this._shadowRoot);
-                this._tagContainer = document.createElement(this._tagType);
-                var theText = document.createTextNode(this._tagText);    
-                this._tagContainer.appendChild(theText); 
-                this._shadowRoot.appendChild(this._tagContainer);
+            if (this._tagContainer){
+                this._tagContainer.parentNode.removeChild(this._tagContainer);
             }
+
+            var shadow = window.getSelection(this._shadowRoot);
+            this._tagContainer = document.createElement(this._tagType);
+            var theText = document.createTextNode(this._tagText);    
+            this._tagContainer.appendChild(theText); 
+            this._shadowRoot.appendChild(this._tagContainer);
 
         }
     
